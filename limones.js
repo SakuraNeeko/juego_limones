7741242fb0bbba8e966cd072ed5269b2514b35a0
SakuraNeeko <bryan_zac22@hotmail.com>
2026-04-16 15:00:35 -0500

    feat: lemon catch

--- limones.js
+++ limones.js
@@ -6,6 +6,7 @@ const ANCHO_PERSONAJE=40;
 const ANCHO_LIMON=20;
 const ALTO_LIMON=20;
 let personajeX=canvas.width/2;
+let personajeY=canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE);
 let limonX=canvas.width/2;
 let limonY=5;
 
@@ -20,12 +21,13 @@ function dibujarSuelo(){
 
 function dibujarPersonaje(){
     ctx.fillStyle="yellow";
-    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),40,ALTURA_PERSONAJE);
+    ctx.fillRect(personajeX,personajeY,ANCHO_PERSONAJE,ALTURA_PERSONAJE);
 }
 
 function moverIzquierda(){
     personajeX=personajeX-10;
     actualizarPantalla();
+    detectarColision();
 }
 
 function actualizarPantalla(){
@@ -38,6 +40,7 @@ function actualizarPantalla(){
 function moverDerecha(){
     personajeX=personajeX+10;
     actualizarPantalla();
+    detectarColision();
 }
 
 function limpiarCanva(){
@@ -52,4 +55,13 @@ function dibujarLimon(){
 function bajarLimon(){
     limonY = limonY + 10;
     actualizarPantalla();
+}
+
+function detectarColision(){
+    if(limonX+ANCHO_LIMON >personajeX && 
+        limonX<personajeX+ANCHO_PERSONAJE &&
+        limonY+ALTO_LIMON>personajeY &&
+        limonY<personajeY+ALTURA_PERSONAJE){
+        alert("atrapado")
+    }
 }
