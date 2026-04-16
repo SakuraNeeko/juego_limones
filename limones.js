3681f5c62c4f743080b6d748c5685c2f21427209
SakuraNeeko <bryan_zac22@hotmail.com>
2026-04-15 21:46:36 -0500

    feat: lemon movement

--- limones.js
+++ limones.js
@@ -3,11 +3,14 @@ let ctx=canvas.getContext("2d");
 const ALTURA_SUELO=40;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
+const ANCHO_LIMON=20;
+const ALTO_LIMON=20;
 let personajeX=canvas.width/2;
+let limonX=canvas.width/2;
+let limonY=5;
 
 function iniciar(){ 
-    dibujarSuelo();
-    dibujarPersonaje();
+    actualizarPantalla();
 }
 
 function dibujarSuelo(){
@@ -22,11 +25,31 @@ function dibujarPersonaje(){
 
 function moverIzquierda(){
     personajeX=personajeX-10;
+    actualizarPantalla();
+}
+
+function actualizarPantalla(){
     limpiarCanva();
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimon();
+}
+
+function moverDerecha(){
+    personajeX=personajeX+10;
+    actualizarPantalla();
 }
 
 function limpiarCanva(){
     ctx.clearRect(0,0,canvas.width, canvas.height);
+}
+
+function dibujarLimon(){
+    ctx.fillStyle="green";
+    ctx.fillRect(limonX,limonY,ANCHO_LIMON,ALTO_LIMON);
+}
+
+function bajarLimon(){
+    limonY = limonY + 10;
+    actualizarPantalla();
 }
